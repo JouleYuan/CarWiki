10602630169d27baa0668b131f40f63ba48eaeb4
JouleYuan <yuanhaoran0318@gmail.com>
2021-07-09 16:27:53 +0800

    Init Spring Boot

--- backend/src/main/java/cn/edu/zju/carwiki/CarWikiApplication.java
+++ backend/src/main/java/cn/edu/zju/carwiki/CarWikiApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CarwikiApplication {
+public class CarWikiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CarwikiApplication.class, args);
+		SpringApplication.run(CarWikiApplication.class, args);
 	}
 
 }
