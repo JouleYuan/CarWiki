72d8d26ebe7e5f06cbe2f59616c14bc9ae1c4217
JouleYuan <yuanhaoran0318@gmail.com>
2021-07-09 16:26:49 +0800

    Init Spring Boot

---
+++ backend/src/main/java/cn/edu/zju/carwiki/CarwikiApplication.java
@@ -0,0 +1,13 @@
+package cn.edu.zju.carwiki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarwikiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarwikiApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/cn/edu/zju/carwiki/CarwikiApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.edu.zju.carwiki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarwikiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
